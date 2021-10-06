b9ef0fe881f596e819304f434ddb31e85ae694e1
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-10-06 04:46:04 -0700

    Updated HOF exerice to perform actual sorting with built-in sorting algorithm.

--- Day3/HigherOrderFunctions/src/main/scala/org/tektutor/SortingAlgorithm.scala
+++ Day3/HigherOrderFunctions/src/main/scala/org/tektutor/SortingAlgorithm.scala
@@ -1,21 +1,18 @@
 package org.tektutor
 
 object Main1:
-
   def heapSort( v : List[Int] ) : List[Int] = 
     println ( "Heap Sort algorthm is used to sort the list")
     //v is the input list
-    //newList is the sorted one
-    val newList = v
-    return v
+    val sortedList = v.sorted
+    println ( sortedList )
+    return sortedList
 
   def mergeSort( v : List[Int] ) : List[Int] = 
     println ( "Merge Sort alogorthm is used to sort the list")
     //v is the input list
     //newList is the sorted one
-    val newList = v
-    //Assuming the newList is sorted
-    return v
+    return v.sorted
   
   //List[Int] => List[Int] tells List[Input] is the input arguments taken by the function
   //List[Int] after the => indicates the fuctions return List[Int]
@@ -25,12 +22,16 @@ object Main1:
 
   //Higher Order Function
   //f:() => String - The function takes zero argument but return a String 
-  //f:(Int) => Int - This function takes one Int and Return an Int 
-
+  //Return type - f:(Int) => Int - This function takes one Int and Return an Int 
   def sort ( l : List[Int], f: () =>  List[Int] => List[Int] ): Unit = 
     val sortingAlgo = f( )  // This will invoke suggestAlgo method 
     val sortedList  = sortingAlgo( l ) //This might be a call to heapSort, mergeSort, etc/
 
   @main def demonstrateHOF1() = 
     val l = List(10,1,12)
-    sort ( l, suggestAlgo ) 
+
+    println ("\nBefore sorting ..." )
+    println ( l )
+
+    println ( "\nAfter sorting ..." )
+    val sortedList = sort ( l, suggestAlgo ) 
